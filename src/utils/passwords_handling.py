02228bba4b600c24f456950c43e36862a9c2aea7
Samarth <samarth38work@gmail.com>
2025-03-04 06:00:09 +0000

    Minor updates on password handling methods

--- src/utils/passwords_handling.py
+++ src/utils/passwords_handling.py
@@ -1,19 +1,55 @@
+from datetime import datetime, timedelta, timezone
 import secrets
 from passlib.context import CryptContext
 
 class PasswordHandle:
+    """
+    Class to handle password hashing and verification
+    """
     def __init__(self):
         self.pwd_context = CryptContext(
-            schemes=["argon2", "bcrypt"], 
+            schemes=["argon2", "bcrypt"],
             default="argon2",
             deprecated="auto"
         )
 
-    def hashpassword(self, password: str) -> str:
+    def hash_password(self, password: str) -> str:
+        """
+        This method hashes a password
+        Args:
+            password (str): The password to hash
+        Returns:
+            str: The hashed password
+        """
         return self.pwd_context.hash(password)
-    
+
     def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        """
+        Method to verify a password
+        Args:
+            plain_password (str): The plain password
+            hashed_password (str): The hashed password
+        Returns:
+            bool: True if the password is verified, False otherwise
+        """
         return self.pwd_context.verify(plain_password, hashed_password)
-    
-    def generate_jwt_token(self, user_id: int) -> str:
-        return secrets.token_urlsafe(32)
+
+    def generate_reset_token(self, expiry_time: int = 30,
+                            email: str|None = None,
+                            user_id: int|None = None
+                        ) -> tuple: 
+        """
+        Generate a secure token to reset a password with expiration time
+        Args:
+            expiry_time (int): The time in minutes before the token expires
+            email (str): The email of the user
+            user_id (int): The user ID
+        Returns:
+            tuple: The token and the expiration time
+        """
+        if not email and not user_id:
+            raise ValueError("You must provide an email or user_id")
+        
+        token = secrets.token_urlsafe(32)
+        expiration_time = datetime.now(timezone.utc) + timedelta(minutes=expiry_time)
+        return token, expiration_time
